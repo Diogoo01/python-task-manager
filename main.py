bb0aea1ff5d60fe2200775d6f1308a98ccc72903
Diogo <diogo.abrantes24@gmail.com>
2026-08-10 16:10:43 +0100

    Add task creation and listing

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ print("3 - Concluir Tarefa")
 print("4 - Remover Tarefa")
 print("0 - Sair\n")
 
+tarefas = []
+i = 1
+
 while True:
+
     escolha = int(input("Escolha: "))
     
     if escolha == 1:
-        print("Mostrar Tarefas")
+        if not tarefas:
+            print("Não existem tarefas.")
+        else:
+            for tarefa in tarefas:
+                print(i, "-", tarefa)
+                i += 1
     elif escolha == 2:
-        print("Adicionar Tarefa escolhido")
+        tarefa_adicionada = input("Insira o nome da tarefa que pretende adicionar: ")
+        tarefas.append(tarefa_adicionada)
+        print("Tarefa Adicionada com sucesso!")
+
     elif escolha == 3:
         print("Tarefa concluida")
     elif escolha == 4:
